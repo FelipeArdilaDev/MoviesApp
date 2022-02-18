c916ebb43ec9ed466e9cb07e0a08bfa0660a1a06
Felipe Ardila <pipe.ardila2027@gmail.com>
2022-02-18 08:44:52 -0500

    Init commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+import 'package:peliculas/search/search_delegate.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+    print(moviesProvider.onDisplayMovies);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Peliculas en cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search_outlined),
+            onPressed: () =>
+                showSearch(context: context, delegate: MovieSearchDelegate()),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Tarjetas principales
+            CardSwiper(movies: moviesProvider.onDisplayMovies),
+
+            //sliders de peliculas
+            MovieSlider(
+              movies: moviesProvider.popularMovies, //populares
+              title: 'Populares', //opcional
+              onNextPage: () => moviesProvider.getPopularMovies(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
